00cb92e530e8b597034390b9845f435323fd7d5c
Carles Arnal <carlesarnal92@gmail.com>
2024-01-18 10:50:58 +0100

    Automatically delete customers

--- src/main/java/io/apicurio/registry/probe/kafka/ConsumedCustomersResource.java
+++ src/main/java/io/apicurio/registry/probe/kafka/ConsumedCustomersResource.java
@@ -2,19 +2,18 @@ package io.apicurio.registry.probe.kafka;
 
 import io.apicurio.registry.probe.persistence.CustomerEntity;
 import io.apicurio.registry.probe.smoke.ProbeMonitoring;
+import io.quarkus.runtime.StartupEvent;
 import io.smallrye.mutiny.Multi;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
+import jakarta.enterprise.event.Observes;
 import org.eclipse.microprofile.reactive.messaging.Channel;
-import org.jboss.resteasy.reactive.RestSseElementType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @ApplicationScoped
-@Path("/consumed-customers")
 public class ConsumedCustomersResource {
 
     private static final Logger log = LoggerFactory.getLogger(ProbeMonitoring.class);
@@ -22,13 +21,30 @@ public class ConsumedCustomersResource {
     @Channel("customers-from-kafka")
     Multi<CustomerEntity> customers;
 
-    @GET
-    @Produces(MediaType.SERVER_SENT_EVENTS)
-    @RestSseElementType(MediaType.TEXT_PLAIN)
-    public Multi<String> stream() {
-        return customers.map(customer -> {
+    public void startMonitoring(@Observes StartupEvent startupEvent) {
+        int concurrentTasks = 2;
+
+        try {
+            concurrentTasks = Integer.parseInt(System.getenv("CONCURRENT_TASKS"));
+        } catch (Exception e) {
+            log.warn("Cannot load concurrent tasks environment variable", e);
+        }
+
+        ExecutorService e = Executors.newFixedThreadPool(concurrentTasks);
+
+        for (int i = 0; i < concurrentTasks; i++) {
+            e.submit(() -> {
+                log.info("Removing customers...");
+                removeCustomers();
+            });
+        }
+    }
+
+    public void removeCustomers() {
+        customers.map(customer -> {
             final String stringToReturn = String.format("'%s' from %s", customer.getFirstName(), customer.getEmail());
             try {
+                log.info("Deleting customer with email: {}", customer.getEmail());
                 customer.delete();
             } catch (Exception e) {
                 log.error("Exception detected in the Probe application: {}", e.getCause(), e);
